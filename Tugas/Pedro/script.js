63f51e5916770a0763b6d968244eaeba94cdf7c4
Muhammad Fajri <160698852+Fajri135@users.noreply.github.com>
2025-04-24 21:49:43 +0700

    Add files via upload

---
+++ Tugas/Pedro/script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+  alert("Pedro Raccon");
+});
+
+audio.addEventListener("click", function audio() {
+  alert("Pedro, Pedro il procione!");
+});
+
+gambar.addEventListener("click", function gambar() {
+  alert("Ciao a tutti, Mi chiamo Pedro il procione!");
+});
